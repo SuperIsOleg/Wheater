286aac56600143be879f4b2bff2fff94c0ffe269
Oleg Kalistratov <oleg.K@northitgroup.eu>
2023-06-17 18:45:16 +0300

    init application and pod install

---
+++ Wheater/Wheater/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wheater
+//
+//  Created by Oleg Kalistratov on 17.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
